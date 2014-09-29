9ef8cf10e3683515025731751c6a6b2142fe90bd
Savaş Can ALTUN <savascanaltun@gmail.com>
2014-09-29 09:52:12 +0300

    Update easyCurl.php
    oop desing

--- example/easyCurl.php
+++ example/easyCurl.php
@@ -4,8 +4,8 @@
 * Web : http://savascanaltun.com.tr
 * Mail : savascanaltun@gmail.com
 * GİT : http://github.com/saltun
-* Date : 22.06.2014
-* Update : 27.08.2014
+* Date : 27.08.2014
+* Update : 29.08.2014
 */
 
 cLass easyCurl{
@@ -18,48 +18,54 @@ cLass easyCurl{
   public $ssl_verifypeer=false;
   public $ssl_verifyhost=false;
   public $cookie=false;
+  private $curl;
 
   public function __construct(){
         if (!extension_loaded('curl')) {
-          die('plase CURL İnstall');
+          die('CURL extension not found!');
         }
    }
+
+  public function init($url,$proxy){
+
+    $this->curl  = curl_init();
+    curl_setopt($this->curl, CURLOPT_URL, $url);
+    curl_setopt($this->curl, CURLOPT_USERAGENT, getenv('USER_AGENT'));
+    curl_setopt($this->curl, CURLOPT_TIMEOUT, $this->timeout);
+    curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($this->curl, CURLOPT_FOLLOWLOCATION, $this->followlocation);
+    curl_setopt($this->curl, CURLOPT_REFERER, $this->referer);
+    if ($proxy){
+      $proxy = explode(':', $proxy);
+      curl_setopt($this->curl, CURLOPT_PROXY, $proxy[0]);
+      curl_setopt($this->curl, CURLOPT_PROXYPORT, $proxy[1] );
+    }
+    curl_setopt($this->curl, CURLOPT_BINARYTRANSFER, TRUE);
+    curl_setopt($this->curl, CURLOPT_HEADER, $this->header);
+
+
+       
+  }
 
 
 
   public function SourceCode($url,$proxy=NULL){
-        $user = getenv('USER_AGENT');
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_USERAGENT, $user);
-        curl_setopt($curl, CURLOPT_TIMEOUT, $this->timeout);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, $this->followlocation);
-        curl_setopt($curl, CURLOPT_REFERER, $this->referer);
-         if ($proxy) {
-          $proxy=explode(':', $proxy);
-          curl_setopt( $ch , CURLOPT_PROXY , $proxy[0]);
-          curl_setopt( $ch , CURLOPT_PROXYPORT , $proxy[1] );
-        }
-        curl_setopt($curl, CURLOPT_BINARYTRANSFER, true);
-        curl_setopt($curl, CURLOPT_HEADER, $this->header);
-        $data = curl_exec($curl);
-        curl_close($curl);
-
-        if($data) return $data; else return false;
+     $this->init($url, $proxy); 
+         $exec = curl_exec($this->curl);
+        return $exec;     
   }
 
-  public function curlPost($action=NULL,$content=NULL,$proxy=NULL){
+  public function curlPost($url=NULL,$content=NULL,$proxy=NULL){
 
     /* boşmu değil mi diye kontrol ettiriyoruz */
-   if (empty($action)) {
+   if (empty($url)) {
       return "Hata : action Adresi boş";
     }else if(empty($content)){
       return "Hata : Post verileri yok";
     }
 
-       $data=null;
-            if (isset($content)) {
+            $data=null;
+            if (isset($content) && is_array($content)) {
               $count=count($content);
               $keys=array_keys($content);
               $values=array_values($content);
@@ -77,36 +83,12 @@ cLass easyCurl{
                
           }
 
-       $ch = curl_init();
-      
-        curl_setopt($ch, CURLOPT_HEADER,$this->header);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-        curl_setopt($ch, CURLOPT_COOKIESESSION,$this->cookie);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,$this->ssl_verifypeer);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,$this->ssl_verifyhost);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION,$this->followlocation);
-
-         if ($proxy) {
-          $proxy=explode(':', $proxy);
-          curl_setopt( $ch , CURLOPT_PROXY , $proxy[0]);
-          curl_setopt( $ch , CURLOPT_PROXYPORT , $proxy[1] );
-        }
-
-        curl_setopt($ch, CURLOPT_REFERER, $this->referer);
-        curl_setopt($ch, CURLOPT_URL, $action);
-        curl_setopt($ch, CURLOPT_USERAGENT,$_SERVER['HTTP_USER_AGENT']);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
-
-
-
+        $this->init($url, $proxy); 
+        curl_setopt($this->curl, CURLOPT_POST, true);
+        curl_setopt($this->curl, CURLOPT_POSTFIELDS,$data);
  
-        $exec = curl_exec($ch);
+        $exec = curl_exec($this->curl);
         return $exec;
- 
-
-       
-
 
   }
 
