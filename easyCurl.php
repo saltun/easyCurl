00ac7bed37586b501a4e385a2309689bdc3ef43a
Savaş Can ALTUN <savascanaltun@gmail.com>
2016-07-17 01:16:26 +0300

    PHPDOC Add

--- easyCurl.php
+++ easyCurl.php
@@ -1,35 +1,84 @@
 <?php
-/*
-* Author : Savaş Can Altun
-* Web : http://savascanaltun.com.tr
-* Mail : savascanaltun@gmail.com
-* GİT : http://github.com/saltun
-* Date : 27.08.2014
-* Update : 26.06.2016
+/**
+* @author Savaş Can Altun <savascanaltun@gmail.com>
+* @link http://savascanaltun.com.tr
+* @link http://github.com/saltun
+* @since 27.08.2014
+* @version 1.5
 */
 
 cLass easyCurl{
 
-
+  /**
+  * @var string
+  */
   public  $referer="http://google.com";
+
+  /**
+  * @var bool|boolean
+  */
   public  $followlocation=false;
+
+  /**
+  * @var bool|boolean
+  */
   public  $header=false;
+
+  /**
+  * @var int
+  */
   public  $timeout=5;
+
+  /**
+  * @var bool|boolean
+  */
   public  $ssl_verifypeer=false;
+
+  /**
+  * @var bool|boolean
+  */
   public  $ssl_verifyhost=false;
+
+  /**
+  * @var bool|boolean
+  */
   public  $cookie=false;
+
+  /**
+  * @var string
+  */
   public  $error;
+
+  /**
+  * @var int
+  */
   public  $errorNumber;
+
+  /**
+  * @var bool|boolean
+  */
   public  $speed="None";
+
+  /**
+  * @var string
+  */
   private $curl;
 
-  /* run class and curl  control - sınıf başlangıcı ve curl kontrolü */
+  /**
+    Run Class and check curl extension. 
+  */
   public function __construct(){
         if (!extension_loaded('curl')) {
           die('CURL extension not found!');
         }
    }
 
+  /**
+  * CURL Base
+  * @param string
+  * @param string
+  * @return null
+  */
   public function init($url,$proxy){
     $this->curl  = curl_init();
     curl_setopt($this->curl, CURLOPT_URL, $url);
@@ -55,16 +104,32 @@ cLass easyCurl{
   }
 
 
-
+  /**
+  * Get Page source code
+  * @param string 
+  * @param null|string
+  * @return string
+  */
   public function sourceCode($url,$proxy=NULL){
      $this->init($url, $proxy); 
          $exec = curl_exec($this->curl);
         return $exec;     
   }
 
+  /**
+  * Curl page post
+  * @param null|string
+  * @param null|string
+  * @param null|string
+  * @return string
+  *
+  */
+
   public function curlPost($url=NULL,$content=NULL,$proxy=NULL){
 
-      /* boşmu değil mi diye kontrol ettiriyoruz */
+        /**
+        * Check $url varible 
+        */
         if (empty($url)) {
             return "Hata : action Adresi boş";
         } else if(empty($content)) {
@@ -80,7 +145,9 @@ cLass easyCurl{
 
   }
 
-  /* finish class - sınıf sonu */
+  /**
+  * End Class and Curl close 
+  */
   public function __destruct(){
     curl_close($this->curl);
   }
